d5899dc867bdcb2e8ead1a444844b6ae955a9f74
Loki-chen <chenyt_x@163.com>
2024-05-06 18:11:13 +0800

    fix attention include file

--- src/protocols/attention.h
+++ src/protocols/attention.h
@@ -1,36 +1,28 @@
 #ifndef FAST_ATTENTION_H__
 #define FAST_ATTENTION_H__
-#include <utils.h>
-#include <config.h>
-#define SOFTMAX_TIME_TEST
+#include "protocol.h"
+// #define SOFTMAX_TIME_TEST
 class Multi_Head_Attention;
 
-class Attention
+class Attention : public Protocol
 {
-    CKKSKey *party;
-    CKKSEncoder *encoder;
-    SEALContext *context;
-    Evaluator *evaluator;
-    IOPack *io_pack;
-    size_t d_module, d_k, head;
+    int head;
 
 public:
     friend Multi_Head_Attention;
-    double scale = 1ul << 40;
-    Attention(CKKSKey *party_, SEALContext *context, IOPack *io_pack_,
-              size_t d_module_, size_t d_k_, size_t head_);
-    ~Attention();
-    LongCiphertext forward(const std::vector<double> &input);
+    Attention(CKKSKey *party, CKKSEncoder *encoder, Evaluator *evaluator, IOPack *io_pack,
+              size_t head_) : Protocol(party, encoder, evaluator, io_pack), head(head_) {}
+    ~Attention() {}
+    std::vector<double> forward(const std::vector<double> &input) const;
 };
 
-class Multi_Head_Attention
+class Multi_Head_Attention : public Protocol
 {
-public:
     Attention **attns;
-    size_t n_head;
-    Multi_Head_Attention(CKKSKey *party, SEALContext *context, IOPack *io_pack,
-                         size_t n_head_, size_t d_module, size_t d_k);
+
+public:
+    Multi_Head_Attention(CKKSKey *party, CKKSEncoder *encoder, Evaluator *evaluator, IOPack *io_pack);
     ~Multi_Head_Attention();
-    LongCiphertext forward(const std::vector<double> &input);
+    LongCiphertext forward(const std::vector<double> &input) const;
 };
 #endif
--- src/protocols/layer-norm1.h
+++ src/protocols/layer-norm1.h
@@ -1,3 +1,4 @@
+
 #ifndef FAST_LAYER_NROM1_H__
 #define FAST_LAYER_NROM1_H__
 #include "protocol.h"
