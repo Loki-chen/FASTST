81ed3da89578323ef0c78b7a08d803d62944d812
root <schzen>
2024-06-25 21:00:28 +0800

    commit test

--- test/protocols_test/pro_attn_test.cpp
+++ test/protocols_test/pro_attn_test.cpp
@@ -1,7 +1,6 @@
 #include <protocols.h>
 
-int main(int argc, const char **argv)
-{
+int main(int argc, const char **argv) {
     EncryptionParameters parms(scheme_type::ckks);
     parms.set_poly_modulus_degree(poly_modulus_degree);
     parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, {60, 40, 40, 60}));
@@ -9,13 +8,10 @@ int main(int argc, const char **argv)
     CKKSEncoder *encoder = new CKKSEncoder(*context);
     Evaluator *evaluator = new Evaluator(*context);
     int party_ = argc > 1 ? 2 : 1;
-    if (party_ == sci::ALICE)
-    {
+    if (party_ == sci::ALICE) {
         std::cout << "Party: ALICE"
                   << "\n";
-    }
-    else if (party_ == sci::BOB)
-    {
+    } else if (party_ == sci::BOB) {
         std::cout << "Party: BOB"
                   << "\n";
     }
@@ -40,20 +36,13 @@ int main(int argc, const char **argv)
     STOP_TIMER("Multi-Head Attention");
     size_t comm = io_pack->get_comm();
     size_t rounds = io_pack->get_rounds(); // / n_heads;
-    if (comm < 1024)
-    {
+    if (comm < 1024) {
         printf("data size of communication: %ld B\n", comm);
-    }
-    else if (comm < 1024 * 1024)
-    {
+    } else if (comm < 1024 * 1024) {
         printf("data size of communication: %.2lf KB\n", comm / 1024.);
-    }
-    else if (comm < 1024 * 1024 * 1024)
-    {
+    } else if (comm < 1024 * 1024 * 1024) {
         printf("data size of communication: %.2lf MB\n", comm / (1024. * 1024.));
-    }
-    else
-    {
+    } else {
         printf("data size of communication: %.2lf MB\n", comm / (1024. * 1024. * 1024.));
     }
     std::cout << "rounds of communication: " << rounds << "\n";
